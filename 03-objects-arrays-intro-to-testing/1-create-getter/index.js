ac43af676ddf8340a644ce6d6fd697fa69f0f3e1
Dmitry Krylov <mitya.krylov@gmail.com>
2020-08-31 11:33:37 +0300

    createGetter implementation

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const fields = path.split('.');
 
+  return function(obj) {
+    for (const field of fields) {
+      if (obj === undefined) {
+        return undefined;
+      }
+      obj = obj[field];
+    }
+    return obj;
+  };
 }
